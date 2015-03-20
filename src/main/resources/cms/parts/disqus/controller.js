bd062955a803f474063c980fbb2af112e4113418
mla <mla@enonic.com>
2015-03-19 23:12:58 -0700

    Added site base URL config.

--- src/main/resources/cms/parts/disqus/controller.js
+++ src/main/resources/cms/parts/disqus/controller.js
@@ -6,21 +6,18 @@ exports.get = function(req) {
     var component = execute('portal.getComponent');
     var config = component.config;
     var site = execute('portal.getSite');
-    var moduleConfig = site.moduleConfigs[module.name];
+    var moduleConfig = site.moduleConfigs[module.name] || {};
     var disqus = {};
     var style  = null;
 
     disqus.shortname = moduleConfig.shortname? moduleConfig.shortname : 'configure';
     disqus.identifier = content._id;
     disqus.title = content.displayName;
-    disqus.url = execute('portal.pageUrl', {
+    disqus.url = moduleConfig.siteUrl + execute('portal.pageUrl', {
         path: content._path
     });
 
-    stk.log(disqus);
-
-    stk.log('req.mode');
-    stk.log(req.mode);
+    //stk.log(req);
 
     // Ensure that the part can be selected in live-edit.
     if (req.mode == 'edit') {
